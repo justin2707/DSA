a8b8dfa9ea491b55157f011bf1cdf520c7d038e6
justin2707 <39796875+justin2707@users.noreply.github.com>
2024-09-24 12:04:36 -0500

    Create hashmap.py
    This algorithm creates a hashmap with the following functions. Map(),get(),put(),remove(). The hashmap is implemented using an array and LinkedList combination

---
+++ hashmap.py
@@ -0,0 +1,37 @@
+class ListNode:
+  def __init__(self,key=-1,val=-1,next=None):
+       self.key=key
+       self.val=val
+       self.next=next
+
+class HashMap:
+   def __init__(self):
+      self.map=[ListNode() for i in range(1000)]
+
+   def  hash(self,key):
+      return key%len(self.map)
+
+   def  put(self,key,value):
+       cur=self.map[self.hash(key)]
+       while cur.next:
+            if cur.key==key
+                cur.val=value
+            cur=cur.next
+
+   def get(self,key)->int:
+      cur=self.map[self.hash(key)].next
+      while cur:
+         if cur.key==key
+           return cur.val
+          cur=cur.next
+      return -1
+
+   def remove(self,key):
+      cur=self.map[self.hash(key)]
+      while cur and cur.next:
+          if cur.next.key==key:
+            cur.next=cur.next.next
+            return
+      cur=cur.next
+        
+        
